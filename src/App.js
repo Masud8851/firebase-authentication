7d792a8203935e93d2cad75c8fec8a15cf977d32
Masud8851 <masud1971.cse@gmail.com>
2021-10-06 11:31:29 +0600

    Authentication

--- src/App.js
+++ src/App.js
@@ -11,16 +11,27 @@ function App() {
 
 	const handleGoogleSignIn = () => {
 		const auth = getAuth();
-		signInWithPopup(auth, provider).then((result) => {
-			// const loginUser = result.user;
-			const { displayName, email, photoURL } = result.user;
-			const logedInUser = {
-				name: displayName,
-				email: email,
-				photo: photoURL,
-			};
-			setUser(logedInUser);
-		});
+		signInWithPopup(auth, provider)
+			.then((result) => {
+				// const loginUser = result.user;
+				const { displayName, email, photoURL } = result.user;
+				const logedInUser = {
+					name: displayName,
+					email: email,
+					photo: photoURL,
+				};
+				setUser(logedInUser);
+			})
+			.catch((error) => {
+				// Handle Errors here.
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				// The email of the user's account used.
+				const email = error.email;
+				// The AuthCredential type that was used.
+				const credential = GoogleAuthProvider.credentialFromError(error);
+				// ...
+			});
 	};
 	return (
 		<div className="App">
@@ -30,6 +41,7 @@ function App() {
 				<div>
 					<h2>Welcome {user.name}</h2>
 					<p>I know your email address : {user.email}</p>
+					<img src={user.photo} alt="" />
 				</div>
 			)}
 		</div>
