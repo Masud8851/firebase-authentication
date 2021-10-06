cc803dff679388e8d5ef1a523f7e3620fedef54d
Masud8851 <masud1971.cse@gmail.com>
2021-10-06 11:03:45 +0600

    Authentication

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { getAuth, signInWithProvider, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import "./App.css";
 import initializeAuthentication from "./FIrebase/firebase.initialize";
 
@@ -6,9 +6,16 @@ initializeAuthentication();
 const provider = new GoogleAuthProvider();
 
 function App() {
+	const handleGoogleSignIn = () => {
+		const auth = getAuth();
+		signInWithPopup(auth, provider).then((result) => {
+			const user = result.user;
+			console.log(user);
+		});
+	};
 	return (
 		<div className="App">
-			<button>Google Sign in</button>
+			<button onClick={handleGoogleSignIn}>Google Sign in</button>
 		</div>
 	);
 }
