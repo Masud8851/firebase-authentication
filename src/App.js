8eca1e0f8fb30d4a0c8029f1728bfc0e0b183fbc
Masud8851 <masud1971.cse@gmail.com>
2021-10-06 10:54:47 +0600

    Authentication

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { GoogleAuthProvider } from "firebase/auth";
+import "./App.css";
+import initializeAuthentication from "./FIrebase/firebase.initialize";
+
+initializeAuthentication();
+const provider = new GoogleAuthProvider();
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	return <div className="App"></div>;
 }
 
 export default App;
