7495908d69bf78f13b550df7bcc47199b1bb6bdb
Masud8851 <masud1971.cse@gmail.com>
2021-10-06 10:44:16 +0600

    Authentication

---
+++ src/FIrebase/firebase.config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyBbTMvZEzsZ6cxBYKi8hjxfhuf2UVZUEOs",
+	authDomain: "simple-firebase-authintication.firebaseapp.com",
+	projectId: "simple-firebase-authintication",
+	storageBucket: "simple-firebase-authintication.appspot.com",
+	messagingSenderId: "899421406020",
+	appId: "1:899421406020:web:63d47ec5892de69ced8d11",
+};
+
+export default firebaseConfig;
